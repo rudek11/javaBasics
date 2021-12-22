27ab13ca068fc154371a34b9890dfc41734a3d7d
rudek11 <rudek11@interia.pl>
2021-12-22 23:34:23 +0100

    done

--- src/main/java/pl/coderslab/strings/Main01.java
+++ src/main/java/pl/coderslab/strings/Main01.java
@@ -4,7 +4,17 @@ public class Main01 {
 
     public static void main(String[] args) {
 
-    String str = "CodersLab jak dobrze zacząć programować";
+        String str = "CodersLab jak dobrze zacząć programować";
+        String lastChar = str.substring(str.length() - 2, str.length() - 1);
+//        System.out.println(lastChar);
+        char charArray[] = str.toCharArray();
+        int a = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == 'ć') {
+                a++;
+            }
 
+        }
+        System.out.println(a);
     }
 }
--- src/main/java/pl/coderslab/strings/Main02.java
+++ src/main/java/pl/coderslab/strings/Main02.java
@@ -3,11 +3,15 @@ package pl.coderslab.strings;
 public class Main02 {
 
     public static void main(String[] args) {
+        String text = "abcdsa";
+        System.out.println(charPos(text, 's'));
 
 
     }
 
     static int charPos(String str, char c) {
-        return 1;
+        return str.indexOf(String.valueOf(c));
+
+
     }
 }
