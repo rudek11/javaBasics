a453687df1fd1613aa413741eb817ee58cd7627a
rudek11 <rudek11@interia.pl>
2021-12-22 02:44:35 +0100

    connection-trial

--- src/main/java/pl/coderslab/methods/Main01.java
+++ src/main/java/pl/coderslab/methods/Main01.java
@@ -1,7 +1,16 @@
 package pl.coderslab.methods;
 
 public class Main01 {
-
     public static void main(String[] args) {
+        int result1 = multiply(2, 2);
+        int result2 = Main01.multiply(3, 5);
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+
+    public static int multiply(int multipler, int index) {
+        return multipler * index;
     }
 }
--- src/main/java/pl/coderslab/methods/Main02.java
+++ src/main/java/pl/coderslab/methods/Main02.java
@@ -3,5 +3,12 @@ package pl.coderslab.methods;
 public class Main02 {
 
     public static void main(String[] args) {
+        int num = square(44);
+        System.out.println(num);
+    }
+
+    public static int square(int num) {
+        return num * num;
+
     }
 }
